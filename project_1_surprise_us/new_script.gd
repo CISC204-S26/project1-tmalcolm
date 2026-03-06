6e5b7ea9195f8d72ba3cc05bdf2c7b7f5aaa8ee9
Torian10 <tmalcolm@udel.edu>
2026-03-06 11:12:11 -0500

    update one

---
+++ project_1_surprise_us/new_script.gd
@@ -0,0 +1,17 @@
+extends Label
+@export var lines = []
+@export var line_end = 0
+@export var nextscene = ""
+@export var new_text : String
+@export var wait_time : float
+var current_line = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
