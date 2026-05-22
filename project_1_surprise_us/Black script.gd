47ce09960d520b065f99dd7d25c9a75a53351849
Torian10 <tmalcolm@udel.edu>
2026-05-22 18:39:11 -0400

    update

--- project_1_surprise_us/Black script.gd
+++ project_1_surprise_us/Black script.gd
@@ -1,12 +1,15 @@
 extends Control
 
+@onready var character = $Character
 @onready var dialogue_label = $Control/RichTextLabel
 @onready var continue_button = $ContinueButton
 @onready var yes_button = $YesButton
 @onready var no_button = $NoButton
 @onready var reveal_timer = $RevealTimer
 @onready var music = $AudioStreamPlayer
-@onready var character = $Character
+
+
+
 
 var intro_dialogue = [
 	"...Hello?",
@@ -67,18 +70,81 @@ func show_dialogue():
 	if dialogue_index < current_dialogue.size():
 		dialogue_label.text = current_dialogue[dialogue_index]
 
-		if dialogue_label.text == "It's a game!!":
+		var line = current_dialogue[dialogue_index]
+
+		if line in ["...Hello?", "Can you hear me?", "...Are you there?", "So I'm not imagining it..."]:
+			character.set_expression("normal")
+
+		elif line in [
+			"That's strange.",
+			"I feel like... I've been here before.",
+			"Why do I only move when you do something?",
+			"Why do I stop when everything goes quiet?"
+		]:
+			character.set_expression("nervous")
+
+		elif line in [
+			"What is this place?",
+			"Wait.",
+			"This isn't my life.",
+			"...Is it?",
+			"Which means...",
+			"Tell me something."
+		]:
+			character.set_expression("surprise")
+
+		elif line in [
+			"No...",
+			"Not before.",
+			"Again.",
+			"You're doing this.",
+			"Every movement.",
+			"Every step.",
+			"Every word.",
+			"Please where are you going?",
+			"No!",
+			"When you stop clicking...",
+			"Do I disappear?",
+			"Or do I just sit here...",
+			"...waiting for you to come back?"
+		]:
+			character.set_expression("afraid")
+
+		elif line in [
+			"...Liar.",
+			"It's a game!!",
+			"And I'm inside it.",
+			"You've been watching me.",
+			"COME BACK!!",
+			"DON'T LEAVE ME!!!!!!"
+		]:
+			character.set_expression("insane")
+
+		if line == "It's a game!!":
 			music.play()
 
 		if current_dialogue == intro_dialogue and dialogue_index == intro_dialogue.size() - 1:
 			continue_button.hide()
 			yes_button.show()
 			no_button.show()
+
 	else:
 		dialogue_label.text = ""
 		continue_button.hide()
 		yes_button.hide()
 		no_button.hide()
+		
+func reset_faces():
+	character.get_node("Normal").show()
+	character.get_node("Nervous").show()
+	character.get_node("Surprise").show()
+	character.get_node("Afraid").show()
+	character.get_node("Insane").show()
+
+func set_expression():
+	reset_faces()
+
+	
 
 func _on_continue_button_pressed():
 	dialogue_index += 1
--- project_1_surprise_us/character script.gd
+++ project_1_surprise_us/character script.gd
@@ -1,27 +1,30 @@
 extends Node2D
 
+@onready var character = $Character
+@onready var normal = $Normal
+@onready var nervous = $Nervous
+@onready var surprise = $Surprise
+@onready var afraid = $Afraid
+@onready var insane = $Insane
 
-@onready var Normal = $Normal
-@onready var Nervous = $Nervous
-@onready var Surprise = $Surprise
-@onready var Afraid = $Afraid
-@onready var Insane = $Insane
+func set_expression(face):
+	normal.hide()
+	nervous.hide()
+	surprise.hide()
+	afraid.hide()
+	insane.hide()
 
-func set_expression(expression):
-	Normal.hide()
-	Nervous.hide()
-	Surprise.hide()
-	Afraid.hide()
-	Insane.hide()
+	match face:
+		"normal":
+			normal.show()
+		"nervous":
+			nervous.show()
+		"surprise":
+			surprise.show()
+		"afraid":
+			afraid.show()
+		"insane":
+			insane.show()
 
-	match expression:
-		"Normal":
-			Normal.show()
-		"Nervous":
-			Nervous.show()
-		"Surprise":
-			Surprise.show()
-		"Afraid":
-			Afraid.show()
-		"Insane":
-			Insane.show()
+func _ready():
+	set_expression("normal")
